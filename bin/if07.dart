c3262e27089ec156c9d1f9eaf7023b15aef12531
network7277 <husanjurayev7277@gmail.com>
2023-02-27 23:38:32 +0500

    07

--- bin/if07.dart
+++ bin/if07.dart
@@ -11,4 +11,44 @@
         a: integer
     Returns:
         string: the message to print */
-void main() {}
+
+String func( int a ){
+
+    String s1 = "positive even number";
+    String s2 = "positive odd number";
+    String s3 = "negative odd number";
+    String s4 = "the number is zero";
+    String s5 = "negative even number";
+
+    if ( a % 2 == 0 ){
+        return s1;
+    }
+
+    if ( a % 2 == 1 ){
+        return s2;
+    }
+
+    if ( a < 0 && a % 2 == 1 ){
+        return s3;
+    }
+
+   
+    if ( a == 0 ){
+        return s4;
+    }
+
+    if ( a < 0 && a % 2 == 0 ){
+    return s5;
+    }
+
+    return s1;
+    return s2;
+    return s3;
+    return s3;
+    return s4;
+    return s5;
+}
+
+void main() {
+    print(func(57));
+}
