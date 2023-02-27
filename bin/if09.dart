7cc0599c84bbb142fc2f224c3eea96ac72fe537e
network7277 <husanjurayev7277@gmail.com>
2023-02-27 23:44:04 +0500

    09

--- bin/if09.dart
+++ bin/if09.dart
@@ -10,4 +10,22 @@
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False. */
 
-void main() {}
+bool func( int a ){
+    int x1;
+    int x2;
+    int x3;
+
+    x1 = a % 10;
+    x2 = a ~/ 10;
+    x3 = x1 * 10 + x2;
+    
+    if ( x3 <= a ){
+        return true ;
+    }
+
+    return false ;
+}
+
+void main() {
+    print(func(58));
+}
