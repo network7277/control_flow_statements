31c1e694068c247541e96affe7f74f45177e1fba
network7277 <husanjurayev7277@gmail.com>
2023-02-27 23:41:50 +0500

    08

--- bin/if08.dart
+++ bin/if08.dart
@@ -10,5 +10,36 @@
         a: integer
     Returns:
         string: the message to print */
+String func( int a ){
 
-void main() {}
+    String s1 = "two-digit odd number";
+    String s2 = "two-digit even number";
+    String s3 =  "three-digit odd number";
+    String s4 = "three-digit even number";
+
+    if ( a > 9 && a < 100 && a % 2 == 1 ){
+        return  s1;
+    }
+
+    if ( a > 9 && a < 100 && a % 2 == 0 ){
+        return s2;
+    }
+
+    if ( a > 99 && a < 1000 && a % 2 == 1 ){
+        return s3;
+    }
+
+    if ( a > 99 && a < 1000 && a % 2 == 0 ){
+    return s4;
+    }
+
+    return s1;
+    return s2;
+    return s3;
+    return s4;
+    
+}
+
+void main() {
+    print(func( 697 ));
+}
